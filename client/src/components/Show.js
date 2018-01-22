5dd3ac7ba80c01c6a2503dc21367113bbae9166f
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-21 21:06:31 -0500

    Add componentDidMount and update classes

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -1,34 +1,29 @@
 var React = require("react");
 
 class Show extends React.Component {
+  componentDidMount() {
+    this.props.showDirectBook();
+  }
+
   render() {
-    let allBooks = this.props.books;
-    let currentBook;
-
-    for (var i = 0; i < allBooks.length; i++) {
-      if (allBooks[i].id === this.props.bookId) {
-        currentBook = allBooks[i];
-      }
-    }
-
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-4 col-centered">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">{currentBook.title}</h5>
+                <h5 className="card-title">{this.props.title}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">
-                  {currentBook.author}
+                  {this.props.author}
                 </h6>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <a href="#" className="card-link">
+                <a href="" className="card-link">
                   Edit
                 </a>
-                <a href="#" className="card-link">
+                <a href="" className="card-link">
                   Delete
                 </a>
               </div>
