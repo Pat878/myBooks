034ca6ae6ea3944483deb98abe66ffcb0eabb746
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-22 20:48:47 -0500

    Create Edit component

---
+++ client/src/components/Edit.js
@@ -0,0 +1,68 @@
+var React = require("react");
+
+class Edit extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-centered">
+            <div className="card">
+              <div className="card-body">
+                <form>
+                  <div className="form-group">
+                    <label for="exampleInputEmail1">Title</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      value={this.props.title}
+                      onChange={this.props.createTitle}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="exampleInputPassword1">Author</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="exampleInputPassword1"
+                      value={this.props.author}
+                      onChange={this.props.createAuthor}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="exampleFormControlTextarea1">Summary</label>
+                    <textarea
+                      className="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                      value={this.props.summary}
+                      onChange={this.props.createSummary}
+                    />
+                  </div>
+                </form>
+                <div class="btn-group" role="group">
+                  <button
+                    href=""
+                    className="btn btn-success"
+                    onClick={this.props.submitUpdatedBook}
+                  >
+                    Update
+                  </button>
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+              </div>
+
+              <button className="btn btn-default" onClick={this.props.goBack}>
+                Back
+              </button>
+            </div>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+module.exports = Edit;
