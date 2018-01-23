74a6363a19c544e3b9ea3659324f57953f899e4e
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-22 20:49:45 -0500

    Add necessary updates to display Edit component

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -5,6 +5,7 @@ var Switch = require("react-router-dom").Switch;
 
 var Index = require("./Index");
 var Show = require("./Show");
+var Edit = require("./Edit");
 var Nav = require("./Nav");
 var Main = require("./Main");
 var Footer = require("./Footer");
@@ -43,8 +44,35 @@ class Routes extends React.Component {
               goBack={this.props.goBack}
               showloading={this.props.showLoading}
               showDirectBook={this.props.showDirectBook}
+              editBook={this.props.editBook}
             />
           )}
+          <Footer />
+        </div>
+      );
+    };
+
+    const EditRoute = props => {
+      return (
+        <div>
+          <Nav />
+          <Main />
+
+          <Edit
+            books={this.props.books}
+            title={this.props.title}
+            author={this.props.author}
+            summary={this.props.summary}
+            bookId={this.props.bookId}
+            goBack={this.props.goBack}
+            showloading={this.props.showLoading}
+            showDirectBook={this.props.showDirectBook}
+            submitUpdatedBook={this.props.submitUpdatedBook}
+            createTitle={this.props.createTitle}
+            createAuthor={this.props.createAuthor}
+            createSummary={this.props.createSummary}
+          />
+
           <Footer />
         </div>
       );
@@ -57,6 +85,7 @@ class Routes extends React.Component {
             <Switch>
               <Route exact path={"/"} render={IndexRoute} />
               <Route path="/books/:id" render={ShowRoute} />
+              <Route path="/edit/:id" render={EditRoute} />
               <Route
                 render={function() {
                   return <p>Not Found</p>;
