25660080b86cba89c173644b572d5505c67598a9
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-23 19:05:56 -0500

    Change Main to a FSC and update props

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,31 +1,29 @@
 var React = require("react");
 
-class Main extends React.Component {
-  render() {
-    return (
-      <main role="main">
-        <div className="jumbotron">
-          <div className="container">
-            <center>
-              <h1 className="display-3">
-                <b>myBooks</b>
-              </h1>
+const Main = () => {
+  return (
+    <main role="main">
+      <div className="jumbotron">
+        <div className="container">
+          <center>
+            <h1 className="display-3">
+              <b>myBooks</b>
+            </h1>
 
-              <p>
-                myBooks is a simple example of how Create-React-App and Ruby on
-                Rails can work together.
-              </p>
-              <p>
-                <a className="btn btn-primary btn-lg" href="" role="button">
-                  Add Book &raquo;
-                </a>
-              </p>
-            </center>
-          </div>
+            <p>
+              myBooks is a simple example of how Create-React-App and Ruby on
+              Rails can work together.
+            </p>
+            <p>
+              <a className="btn btn-primary btn-lg" href="" role="button">
+                Add Book &raquo;
+              </a>
+            </p>
+          </center>
         </div>
-      </main>
-    );
-  }
-}
+      </div>
+    </main>
+  );
+};
 
 module.exports = Main;
