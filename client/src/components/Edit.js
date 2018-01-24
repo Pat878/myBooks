6f3875dffdefbde05c7dd96cd7ffe8a483bfe63d
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-23 19:04:14 -0500

    Change Edit to a FSC and update props

--- client/src/components/Edit.js
+++ client/src/components/Edit.js
@@ -1,68 +1,69 @@
 var React = require("react");
 
-class Edit extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4 col-centered">
-            <div className="card">
-              <div className="card-body">
-                <form>
-                  <div className="form-group">
-                    <label htmlFor="InputTitle">Title</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      value={this.props.title}
-                      onChange={this.props.createTitle}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="InputAuthor">Author</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="exampleInputPassword1"
-                      value={this.props.author}
-                      onChange={this.props.createAuthor}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="InputSummary">Summary</label>
-                    <textarea
-                      className="form-control"
-                      id="exampleFormControlTextarea1"
-                      rows="3"
-                      value={this.props.summary}
-                      onChange={this.props.createSummary}
-                    />
-                  </div>
-                </form>
-                <div class="btn-group" role="group">
-                  <button
-                    href=""
-                    className="btn btn-success"
-                    onClick={this.props.submitUpdatedBook}
-                  >
-                    Update
-                  </button>
-                  <button className="btn btn-danger">Delete</button>
+const Edit = props => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 col-centered">
+          <div className="card">
+            <div className="card-body">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="InputTitle">Title</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="title"
+                    id="InputTitle"
+                    aria-describedby="emailHelp"
+                    value={props.fields.title}
+                    onChange={props.updateForm}
+                  />
                 </div>
+                <div className="form-group">
+                  <label htmlFor="InputAuthor">Author</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="author"
+                    id="InputAuthor"
+                    value={props.fields.author}
+                    onChange={props.updateForm}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="InputSummary">Summary</label>
+                  <textarea
+                    className="form-control"
+                    name="summary"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    value={props.fields.summary}
+                    onChange={props.updateForm}
+                  />
+                </div>
+              </form>
+              <div class="btn-group" role="group">
+                <button
+                  href=""
+                  className="btn btn-success"
+                  onClick={props.submitUpdatedBook}
+                >
+                  Update
+                </button>
+                <button className="btn btn-danger">Delete</button>
               </div>
-
-              <button className="btn btn-default" onClick={this.props.goBack}>
-                Back
-              </button>
             </div>
+
+            <button className="btn btn-default" onClick={props.goBack}>
+              Back
+            </button>
           </div>
         </div>
-        <hr />
       </div>
-    );
-  }
-}
+      <hr />
+    </div>
+  );
+};
 
 module.exports = Edit;
