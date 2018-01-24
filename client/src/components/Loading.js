49a914c255029706abd26e4b1980790c4e887bc0
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-23 19:05:35 -0500

    Change Loading to a FSC and update props

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,17 +1,15 @@
 var React = require("react");
 
-class Loading extends React.Component {
-  render() {
-    return (
-      <div>
-        <center>
-          <hr />
-          <i className="fa fa-spinner fa-spin fa-3x fa-fw" />
-          <h1>Loading...</h1>
-        </center>
-      </div>
-    );
-  }
-}
+const Loading = props => {
+  return (
+    <div>
+      <center>
+        <hr />
+        <i className="fa fa-spinner fa-spin fa-3x fa-fw" />
+        <h1>Loading...</h1>
+      </center>
+    </div>
+  );
+};
 
 module.exports = Loading;
