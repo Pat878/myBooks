9bac2396891312573d82016eaf78a5a6522fd6c9
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-23 19:04:52 -0500

    Change Index to a FSC and update props

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -1,31 +1,31 @@
 var React = require("react");
 
-class Index extends React.Component {
-  render() {
-    const bookListing = this.props.books.map((book, i) => {
-      return (
-        <div className="col-md-4" key={book.title}>
-          <h2>{book.title}</h2>
-          <p>
-            <a
-              className="btn btn-secondary"
-              role="button"
-              onClick={this.props.showBookOnClick.bind(this, i)}
-            >
-              View details &raquo;
-            </a>
-          </p>
-        </div>
-      );
-    });
-
+const Index = props => {
+  const bookListing = props.books.map((book, i) => {
     return (
-      <div className="container">
-        <div className="row">{bookListing}</div>
-        <hr />
+      <div className="col-md-4" key={book.title}>
+        <h2>{book.title}</h2>
+        <p>
+          <a
+            className="btn btn-secondary"
+            role="button"
+            onClick={props.showBookOnClick.bind(this, i)}
+          >
+            View details &raquo;
+          </a>
+        </p>
       </div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="container">
+      <center>
+        <div className="row">{bookListing}</div>
+        <hr />
+      </center>
+    </div>
+  );
+};
 
 module.exports = Index;
