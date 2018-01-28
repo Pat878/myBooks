3b0c8e672ed33f6887be28b5a71faa867d6da2ae
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-01-28 14:50:02 -0500

    Update tests

--- client/src/components/tests/Create.test.js
+++ client/src/components/tests/Create.test.js
@@ -18,76 +18,82 @@ describe("Create", () => {
     );
   });
 
-  it("should update prop.fields.title on change", () => {
-    const titleInput = wrapper.find("input").first();
-    titleInput.simulate("change", {
-      target: { value: "test" }
+  describe("input tests", () => {
+    it("should update prop.fields.title on change", () => {
+      const titleInput = wrapper.find("input").first();
+      wrapper.props().fields.title = "title";
+      titleInput.simulate("change");
+      wrapper.mount();
+      expect(wrapper.props().updateForm).toHaveBeenCalled();
+      expect(titleInput.instance().value).toEqual("title");
     });
-    expect(wrapper.props().fields.title).toEqual(titleInput.instance().value);
-  });
 
-  it("should update prop.fields.author on change", () => {
-    const authorInput = wrapper.find("input").at(1);
-    authorInput.simulate("change", {
-      target: { value: "test" }
+    it("should update prop.fields.author on change", () => {
+      const authorInput = wrapper.find("input").at(1);
+      wrapper.props().fields.author = "author";
+      authorInput.simulate("change");
+      wrapper.mount();
+      expect(wrapper.props().updateForm).toHaveBeenCalled();
+      expect(authorInput.instance().value).toEqual("author");
     });
-    expect(wrapper.props().fields.author).toEqual(authorInput.instance().value);
-  });
 
-  it("should update prop.fields.summary on change", () => {
-    const summaryInput = wrapper.find("textarea");
-    summaryInput.simulate("change", {
-      target: { value: "test" }
+    it("should update prop.fields.summary on change", () => {
+      const summaryInput = wrapper.find("textarea");
+      wrapper.props().fields.summary = "summary";
+      summaryInput.simulate("change");
+      wrapper.mount();
+      expect(wrapper.props().updateForm).toHaveBeenCalled();
+      expect(summaryInput.instance().value).toEqual("summary");
     });
-    expect(wrapper.props().fields.summary).toEqual(
-      summaryInput.instance().value
-    );
   });
 
-  it("should render errors on submit if no form data was entered", () => {
-    const button = wrapper.find("button").first();
-    const errors = wrapper.find("span");
-    button.simulate(
-      "click",
-      wrapper.setProps({
-        fieldErrors: {
-          title: "Title Required",
-          author: "Author Required",
-          summary: "Summary Required"
-        }
-      })
-    );
-    expect(wrapper.props().submitNewBook).toHaveBeenCalled();
-    wrapper.mount();
-    expect(wrapper.props().fieldErrors.title).toEqual(errors.at(0).text());
-    expect(wrapper.props().fieldErrors.author).toEqual(errors.at(1).text());
-    expect(wrapper.props().fieldErrors.summary).toEqual(errors.at(2).text());
-  });
-
-  it("goBack function should be called on button click", () => {
-    const button = wrapper.find("button").at(1);
-    button.simulate("click");
-    expect(wrapper.props().goBack).toHaveBeenCalled();
-  });
-
-  it("submitNewBook function should be called with props on button click", () => {
-    const button = wrapper.find("button").first();
-    button.simulate(
-      "click",
-      wrapper.setProps({
-        fields: {
-          title: "example title",
-          author: "example author",
-          summary: "example summary"
-        }
-      })
-    );
-    expect(wrapper.props().submitNewBook).toHaveBeenCalled();
-    expect(wrapper.props().fields).toEqual({
-      title: "example title",
-      author: "example author",
-      summary: "example summary"
+  describe("button tests", () => {
+    it("should render errors on submit if no form data was entered", () => {
+      const button = wrapper.find("button").first();
+      const errors = wrapper.find("span");
+      button.simulate(
+        "click",
+        wrapper.setProps({
+          fieldErrors: {
+            title: "Title Required",
+            author: "Author Required",
+            summary: "Summary Required"
+          }
+        })
+      );
+      expect(wrapper.props().submitNewBook).toHaveBeenCalled();
+      wrapper.mount();
+      expect(wrapper.props().fieldErrors.title).toEqual(errors.at(0).text());
+      expect(wrapper.props().fieldErrors.author).toEqual(errors.at(1).text());
+      expect(wrapper.props().fieldErrors.summary).toEqual(errors.at(2).text());
     });
-    expect(wrapper.props().fieldErrors).toEqual({});
+
+    it("goBack function should be called on button click", () => {
+      const button = wrapper.find("button").at(1);
+      button.simulate("click");
+      expect(wrapper.props().goBack).toHaveBeenCalled();
+    });
+
+    it("submitNewBook function should be called with props on button click", () => {
+      const button = wrapper.find("button").first();
+
+      button.simulate(
+        "click",
+        wrapper.setProps({
+          fields: {
+            title: "example title",
+            author: "example author",
+            summary: "example summary"
+          }
+        })
+      );
+      expect(wrapper.props().submitNewBook).toHaveBeenCalled();
+      expect(wrapper.props().fields).toEqual({
+        title: "example title",
+        author: "example author",
+        summary: "example summary"
+      });
+      expect(wrapper.props().fieldErrors).toEqual({});
+    });
   });
 });
