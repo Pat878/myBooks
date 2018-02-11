88a45e7bf3e82065ce0c70cb15bbefb01ef19fe9
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-02-11 09:22:13 -0500

    Remove Nav, Main, and Footer components

--- client/src/components/views/IndexRoute.js
+++ client/src/components/views/IndexRoute.js
@@ -1,21 +1,15 @@
 var React = require("react");
 var Index = require("../Index");
-var Nav = require("../Nav");
-var Main = require("../Main");
-var Footer = require("../Footer");
 var Loading = require("../Loading");
 
 const IndexRoute = props => {
   return (
     <div>
-      <Nav />
-      <Main createBookPath={props.createBookPath} />
       {props.showLoading ? (
         <Loading />
       ) : (
         <Index books={props.books} showBookOnClick={props.showBookOnClick} />
       )}
-      <Footer />
     </div>
   );
 };
